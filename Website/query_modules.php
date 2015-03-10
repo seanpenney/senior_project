aaf9a7f53e42f3d6a9bb6e141e92720fbf7128bb
seanpenney <seanjpenney@gmail.com>
2015-03-10 12:59:33 -0700

    Added files for website

---
+++ Website/query_modules.php
@@ -0,0 +1,36 @@
+<?php
+
+ini_set('display_errors', 'On');
+include('common.inc.php');
+
+$module_name = array_key_exists("name", $_REQUEST) ? $_REQUEST["name"] : 0;
+
+if (!preg_match('/^.[a-zA-z0-9]+.$/', $module_name))
+	die(json_encode(array('message' => 'ERROR', 'code' => 1337)));
+
+$list = array();
+if ($result = $mysqli->query("select * from modules where name = " . "'" . $module_name . "'")) {
+	while ($obj = $result->fetch_object()) {
+		$new_result = $mysqli->query("select avg(rating) as rating_average from ratings where name = " . "'" . $module_name . "'");
+		$new_obj = $new_result->fetch_object();
+		$item = array(
+			"name" => $obj->name,
+			"link" => $obj->link,
+			"functionality" => $obj->functionality,
+			"tag" => $obj->tag,
+			"rating" => number_format((float)$new_obj->rating_average, 2, '.', '')
+		);
+		array_push($list, $item);
+	}
+	$result->close();
+}
+
+header("Content-Type: application/json", true);
+if (empty($list)) {
+	die(json_encode(array('message' => 'ERROR', 'code' => 1337)));
+}
+else {
+	echo json_encode($list);
+}
+$mysqli->close();
+?>
