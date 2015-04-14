ead728616208a45bbc7174dd78b305638ec7b6d5
seanpenney <seanjpenney@gmail.com>
2015-04-13 21:46:14 -0700

    timer and dimmer functional

--- UBC/app/src/main/java/bluetoothcontroller/ubc/BleDeviceControl.java
+++ UBC/app/src/main/java/bluetoothcontroller/ubc/BleDeviceControl.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import android.bluetooth.BluetoothGattCharacteristic;
@@ -183,6 +184,28 @@ public class BleDeviceControl extends Fragment {
             }
         });
 
+        SeekBar seekBar = (SeekBar) view.findViewById(R.id.set_val);
+        final TextView currentSetVal = (TextView) view.findViewById(R.id.current_set_val);
+        currentSetVal.setText(seekBar.getProgress() + "/" + seekBar.getMax());
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                progress = progressValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                currentSetVal.setText(progress + "/" + seekBar.getMax());
+            }
+        });
+
         return view;
     }
 
@@ -280,10 +303,11 @@ public class BleDeviceControl extends Fragment {
         EditText pinInput = (EditText) getActivity().findViewById(R.id.set_pin);
         int pinNumber = Integer.parseInt(pinInput.getText().toString());
 
-        EditText valInput = (EditText) getActivity().findViewById(R.id.set_val);
-        int valNumber = Integer.parseInt(valInput.getText().toString());
+        SeekBar valInput = (SeekBar) getActivity().findViewById(R.id.set_val);
+        float valNumber = valInput.getProgress();
+        valNumber = valNumber * ((float)255/100);
 
-        String message = "<set_pinval>" + String.format("%02d", pinNumber) + String.format("%03d", valNumber);
+        String message = "<set_pinval>" + String.format("%02d", pinNumber) + String.format("%03d", (int)valNumber);
         byte[] value = new byte[0];
         try {
             value = message.getBytes("UTF-8");
